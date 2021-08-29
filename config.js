7b407c297edb06f5a6ef1d38a08845fbcb578ead
Victor Lin <vvictor.llin@gmail.com>
2021-08-29 01:20:44 -0700

    feat: update config domain

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://victorlin.dev',
   pathPrefix: '/',
   title: 'Victor Lin CS',
   subtitle: '',
   copyright: '',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'G-DX1B999KH4',
   useKatex: false,
   menu: [
     {
